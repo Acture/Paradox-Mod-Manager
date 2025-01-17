0c996d1c75f40f51b5ae9194695926c5dbc9963f
acturea <acturea@gmail.com>
2025-01-17 21:07:17 +0800

    style: fix indentation issues across project files
    Signed-off-by: acturea <acturea@gmail.com>

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -2,37 +2,37 @@ use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct GameConfig {
-    game_name: String,
-    game_dir: PathBuf,
-    mod_dir: PathBuf,
+	game_name: String,
+	game_dir: PathBuf,
+	mod_dir: PathBuf,
 }
 
 impl GameConfig {
-    pub(crate) fn new(game_name: String, game_dir: PathBuf, mod_dir: PathBuf) -> Self {
-        Self {
-            game_name,
-            game_dir,
-            mod_dir,
-        }
-    }
+	pub(crate) fn new(game_name: String, game_dir: PathBuf, mod_dir: PathBuf) -> Self {
+		Self {
+			game_name,
+			game_dir,
+			mod_dir,
+		}
+	}
 
-    pub fn is_game_dir_exists(&self) -> bool {
-        self.game_dir.exists()
-    }
+	pub fn is_game_dir_exists(&self) -> bool {
+		self.game_dir.exists()
+	}
 
-    pub fn is_mod_dir_exists(&self) -> bool {
-        self.mod_dir.exists()
-    }
+	pub fn is_mod_dir_exists(&self) -> bool {
+		self.mod_dir.exists()
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_config_struct() {
-        let config = GameConfig::new("test".to_string(), PathBuf::from("."), PathBuf::from("src"));
-        assert!(config.is_game_dir_exists());
-        assert!(config.is_mod_dir_exists());
-    }
+	#[test]
+	fn test_config_struct() {
+		let config = GameConfig::new("test".to_string(), PathBuf::from("."), PathBuf::from("src"));
+		assert!(config.is_game_dir_exists());
+		assert!(config.is_mod_dir_exists());
+	}
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,46 +6,46 @@ use std::sync::LazyLock;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+	format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 static GAME_CONFIG: LazyLock<DashMap<String, config::GameConfig>> =
-    LazyLock::new(|| DashMap::new());
+	LazyLock::new(|| DashMap::new());
 
 #[tauri::command]
 fn setup_game_config(game_name: &str, game_dir: &str, mod_dir: &str) {
-    let game_dir = std::path::PathBuf::from(game_dir);
-    let mod_dir = std::path::PathBuf::from(mod_dir);
-    let game_config = config::GameConfig::new(game_name.to_string(), game_dir, mod_dir);
-    GAME_CONFIG.insert(game_name.to_string(), game_config);
+	let game_dir = std::path::PathBuf::from(game_dir);
+	let mod_dir = std::path::PathBuf::from(mod_dir);
+	let game_config = config::GameConfig::new(game_name.to_string(), game_dir, mod_dir);
+	GAME_CONFIG.insert(game_name.to_string(), game_config);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+	tauri::Builder::default()
+			.plugin(tauri_plugin_shell::init())
+			.invoke_handler(tauri::generate_handler![greet])
+			.run(tauri::generate_context!())
+			.expect("error while running tauri application");
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_greet() {
-        assert_eq!(
-            greet("world"),
-            "Hello, world! You've been greeted from Rust!"
-        );
-    }
+	#[test]
+	fn test_greet() {
+		assert_eq!(
+			greet("world"),
+			"Hello, world! You've been greeted from Rust!"
+		);
+	}
 
-    #[test]
-    fn test_setup_game_config() {
-        setup_game_config("test", ".", "src");
-        let binding = GAME_CONFIG.get("test").unwrap();
-        let game_config = binding.value();
-        println!("{:?}", game_config);
-    }
+	#[test]
+	fn test_setup_game_config() {
+		setup_game_config("test", ".", "src");
+		let binding = GAME_CONFIG.get("test").unwrap();
+		let game_config = binding.value();
+		println!("{:?}", game_config);
+	}
 }
